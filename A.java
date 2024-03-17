e63da57ff4caa04854fe7babd10441e37f441f12
HarshalGourkar <harshalgourkar1804@gmail.com>
2024-03-17 20:20:40 +0530

    all commit

---
+++ A.java
@@ -0,0 +1,10 @@
+public class A{
+String s;
+public A( String s){
+this.s=s;
+}
+public static void main(String arg[]){
+System.out.println("hello java");
+}
+
+}
---
+++ main/B.java
@@ -0,0 +1,11 @@
+public class B{
+
+int i=2;
+public b(int i)
+{
+this.i=i;
+}
+public void m1(){
+System.out.println("variable i"+i);
+}
+}
